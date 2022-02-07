1545ab7cc7dd8679f910f308f1733d366144384b
welnovaesm <wel.miro@hotmail.com>
2022-02-07 16:06:48 -0300

    Criando Threads em java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Este é o  thread main");
+
+        Thread.sleep(30000);
+    }
+}
